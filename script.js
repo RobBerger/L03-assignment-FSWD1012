9b391c08dd1bc89cd4ce8c19724a3a17e1518434
berger <robertberger.dev@gmail.com>
2022-12-17 22:12:29 -0500

    finished decrement() function

--- script.js
+++ script.js
@@ -11,5 +11,13 @@ function increment() {
 }
 
 function decrement() {
-    
+    var counter = document.getElementById('counter').innerHTML;
+    var currentCount = parseInt(counter);
+
+    document.getElementById('counter').innerHTML = currentCount - 1;
+
+    const counterLogsP = document.createElement('p');
+    counterLogsP.innerHTML = 'The counter has been decreased by 1';
+    var counterParent = document.getElementById('counterLogs');
+    counterParent.appendChild(counterLogsP);
 }
